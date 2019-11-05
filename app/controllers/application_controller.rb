8b30098fc5315e9cfa006520e1e2671545578ba1
macarena peralta <mperalta.costoya@gmail.com>
2019-11-05 19:20:25 -0300

    add-authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  
+  def not_found
+    render json: { errors: 'Not found' }, status: :not_found
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    unless header.nil?
+      header = header.split(' ').last
+      begin
+        @decoded = JsonWebToken.decode(header)
+        @current_user = User.find(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+      end
+    else
+      render json: { errors: 'Authorization header missing' }, status: :bad_request if header.nil?
+    end
+  end
 end
---
+++ app/views/users/_user.json.jbuilder
@@ -0,0 +1,7 @@
+json.id user.id
+json.name user.name
+json.lastname user.lastname
+json.email user.email
+json.birthday user.birthday
+json.address user.address
+json.created_at user.created_at
